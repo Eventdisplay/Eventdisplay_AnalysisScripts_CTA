e26d2f5708bec2faf94a471a7b8b2c5cca968262
GernotMaier <gernot.maier@desy.de>
2020-06-24 15:14:59 +0200

    Macro to mergie all graphs into one file

---
+++ IPRgeneration/mergeIPRGraphs.C
@@ -0,0 +1,50 @@
+/*  script to merge IPR graphs
+ *  produced for each telescopes into
+ *  a singel file as required for the
+ *  eventdisplay analysis
+ *
+ *  root -l -q -b 'mergeIPRGraphs.C( "output.root", "inputdirectory/" )'
+ *
+ */
+
+void mergeIPRGraphs( string iMergedFile = "prod5-IPR.root",
+                     string iUnMergedFileDirectory = "./" )
+{
+     TFile *f = new TFile( iMergedFile.c_str(), "RECREATE" );
+     if( f->IsZombie() )
+     {
+         cout << "Error creating new IPR graph file " << iMergedFile << endl;
+         return;
+     }
+
+     vector< string > fTelTypes;
+     fTelTypes.push_back( "lst" );
+     fTelTypes.push_back( "mst-fc" );
+     fTelTypes.push_back( "mst-nc" );
+     fTelTypes.push_back( "sst" );
+
+     for( unsigned int i = 0; i < fTelTypes.size(); i++ )
+     {
+          string iName = iUnMergedFileDirectory + "./pedestals-" + fTelTypes[i] + ".root";
+          TFile *iF = new TFile( iName.c_str() );
+          if( iF->IsZombie() )
+          {
+              continue;
+          }
+          TIter next(iF->GetListOfKeys());
+          TKey *key = 0;
+          while ((key = (TKey*)next()))
+          {
+               string iClassName = key->GetClassName();
+               if( iClassName == "TGraphErrors" )
+               {
+                   TGraphErrors* iO = ( TGraphErrors* )key->ReadObj();
+                   f->cd();
+                   iO->Write();
+                   iF->cd();
+               }
+          }
+      }
+      f->Close();
+}
+
