1952b1e2768d8a661a45510c31ea24b2d151484f
GernotMaier <gernot.maier@desy.de>
2020-08-24 08:22:30 +0200

    increase memory request for TMVA

--- analysis/CTA.TMVA.sub_train.sh
+++ analysis/CTA.TMVA.sub_train.sh
@@ -315,6 +315,7 @@ echo "* ENERGYBINS 1 ${EMIN[$i]} ${EMAX[$i]}
      echo "SCRIPT $FNAM.sh"
 
      MEM=8000M
+     MEM=12000M
      #################################
      # submit job to queue (for all energy bins)
      qsub $QSUBOPT -V -t 1-$NENE:1 -l h_cpu=47:59:00 -l h_rss=${MEM} -l tmpdir_size=1G -o $QLOG -e $QLOG "$FNAM.sh"
