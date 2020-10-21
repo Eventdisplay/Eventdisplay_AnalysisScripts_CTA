095c84cc3a2e5828a33b7ab4137b59e5179effb0
GernotMaier <gernot.maier@desy.de>
2020-10-21 18:23:36 +0200

    minor update in layouts chosen

--- CTA.runAnalysis.sh
+++ CTA.runAnalysis.sh
@@ -168,7 +168,7 @@ then
        EDM=( "-sq07-LL" )
        ARRAY=( "subArray.prod5.North-XST.list" )
        if [[ $P2 == *"LST"* ]]; then
-           ARRAY=( "subArray.prod5.North-LST.list" )
+           ARRAY=( "subArray.prod5.North-LSTX.list" )
        fi
    else
        echo "DSet not found: $P2"
@@ -183,6 +183,10 @@ then
        TMVADATE="${ANADATE}"
        EFFDATE="${TMVADATE}"
        ARRAY=( "subArray.prod5.North-Hyper.list" )
+       ARRAY=( "subArray.prod5.North-BL.list" )
+       if [[ $P2 == *"LST"* ]]; then
+           ARRAY=( "subArray.prod5.North-LST.list" )
+       fi
    else
        SITE=( "prod5-LaPalma-20deg" )
    fi
@@ -213,6 +217,7 @@ then
    elif [[ $P2 ==  *"sq07"* ]]; then
        EDM=( "-sq07-LL" )
        ARRAY=( "subArray.prod5.South-XST.list" )
+       ARRAY=( "subArray.prod5.South-TS.list" )
        if [[ $P2 == *"LST"* ]]; then
            ARRAY=( "subArray.prod5.South-LST.list" )
        fi
@@ -225,11 +230,15 @@ then
    fi
    if [[ $P2 == *"moon"* ]]; then
        SITE=( "prod5-Paranal-20deg-NSB5x" )
+       ARRAY=( "subArray.prod5.South-Hyper.list" )
+       ARRAY=( "subArray.prod5.South.list" )
    fi
    ARRAYDIR=( "prod5" )
    TDATE="g20210909"
    ANADATE="g20201012"
+   ANADATE="g20200817"
    TMVADATE="${ANADATE}"
+   TMVADATE="g20201012"
    EFFDATE="${ANADATE}"
 else
    echo "error: unknown site; allowed are N or S/S40deg/S60deg"
@@ -242,9 +251,10 @@ OFFAXIS="cone"
 #####################################
 # particle types
 PARTICLE=( "gamma_onSource" "electron" "proton" )
+PARTICLE=( "gamma_cone" )
+PARTICLE=( "gamma_cone" "gamma_onSource" "electron" )
 PARTICLE=( "proton" )
 PARTICLE=( "gamma_cone" "gamma_onSource" "electron" "proton" )
-PARTICLE=( "gamma_cone" )
 
 #####################################
 # cut on number of images
