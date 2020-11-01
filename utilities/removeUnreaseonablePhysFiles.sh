0d6c37a71b114f6c6b8b1168f0d8badb7596fada
GernotMaier <gernot.maier@desy.de>
2020-11-01 20:08:17 +0100

    script to remove empty phys files

---
+++ utilities/removeUnreaseonablePhysFiles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# remove PHYS files which are definitely empty
+# e.g., LST4 for 2LST arrays
+#
+if [ ! -n "$1" ]
+then
+    echo "./removeUnreaseonablePhysFiles.sh <path>"
+    echo
+    echo
+    exit
+fi
+
+if [[ ! -d "$1" ]]; then
+  echo "Error, directory does not exist" 
+  exit
+fi
+
+OPTION="-delete -print"
+
+find "$1" -name "*NIM4LST4*3LSTs00MSTs-*" -print -delete
+find "$1" -name "*NIM4LST4*2LSTs00MSTs-*" -print -delete
+find "$1" -name "*NIM3LST3*2LSTs00MSTs-*" -print -delete
+find "$1" -name "*NIM3LST3MST3*2LSTs03MSTs*" -print -delete
+find "$1" -name "*NIM4LST4MST4*2LSTs03MSTs*" -print -delete
