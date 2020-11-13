630e90ad54e284e5bdd21f419cc6dca855327fac
GernotMaier <gernot.maier@desy.de>
2020-11-13 09:13:55 +0100

    file list preparation

---
+++ utilities/README.md
@@ -0,0 +1,10 @@
+# Utilitity scripts
+
+## File list preparation
+
+Prepare a list of all *sim_telarray* files to be used in evndisp
+
+Example:
+```
+./prepareSimulationFileLists.sh /lustre/fs24/group/cta/prod3b/CTA-ProdX-Download-DESY/Prod3b_Paranal_20deg_HB9/ /lustre/fs21/group/cta/users/maierg/analysis/AnalysisData/FileList_prod3b/prod3b-paranal20deg_SCT/
+```
---
+++ utilities/prepareSimulationFileLists.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# script to prepare list of MC files from a local directory
+#
+#
+
+if [[ $# < 2 ]]; then
+# begin help message
+echo "
+Prepare list of MC files from a local directory
+
+./prepareSimulationFileLists.sh <directory with MC files> <directory to write file lists>
+
+expect subdirectories for particle types
+"
+exit
+fi
+
+MCDIR=${1}
+ODIR=${2}
+
+# output directory
+mkdir -p ${2}
+
+
+for P in gamma_cone gamma_onSource proton electron
+do
+   if [[ -e ${ODIR}/${P}.list ]]; then
+      echo "Output file list ${ODIR}/${P}.list; please remove"
+      exit
+   fi
+
+   echo "Filling list for ${P}:"
+   echo "   from: ${MCDIR}/${P}/"
+   echo "   into: ${ODIR}/${P}.list"
+
+   find "${MCDIR}/${P}/" -name "*simtel*" > ${ODIR}/${P}.list
+
+   echo "total number of files found: "
+   wc -l ${ODIR}/${P}.list
+done
