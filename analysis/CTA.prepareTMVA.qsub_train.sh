acd86eee20d9140ee8994382ee873b5c55512709
GernotMaier <gernot.maier@desy.de>
2021-02-24 17:24:13 +0100

    efficiency improvement; write training events

---
+++ analysis/CTA.prepareTMVA.qsub_train.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# script to prepare trianing events
+#
+#
+#
+
+PFIL=RUNPARA
+OFIL=OOOFILE
+ulimit -n 2056
+
+# set the right observatory (environmental variables)
+source "${EVNDISPSYS}"/setObservatory.sh CTA
+
+echo ${PFIL}.runparameter
+rm -f $PFIL.log
+
+${EVNDISPSYS}/bin/trainTMVAforGammaHadronSeparation "${PFIL}".runparameter WRITETRAININGEVENTS > "${PFIL}".log
+
+# mv log file into root file
+if [ -e ${PFIL}.log ] && [ -e ${OFIL}.root ]
+then
+    ${EVNDISPSYS}/bin/logFile tmvaPrepareLog ${OFIL}.root ${PFIL}.log
+    rm -f ${PFIL}.log
+fi
+if [ -e ${PFIL}.runparameter ]
+then
+    ${EVNDISPSYS}/bin/logFile tmvaPrepareRunparameter ${OFIL}.root ${PFIL}.runparameter
+    rm -f ${PFIL}.runparameter
+fi
+
+exit
