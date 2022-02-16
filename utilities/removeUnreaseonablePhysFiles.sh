5403493ede133d93486b76ff19517898f86b3c57
GernotMaier <gernot.maier@desy.de>
2022-02-16 07:54:47 +0100

    add some file options with multiplicity 6

--- utilities/removeUnreaseonablePhysFiles.sh
+++ utilities/removeUnreaseonablePhysFiles.sh
@@ -17,21 +17,33 @@ if [[ ! -d "$1" ]]; then
 fi
 
 #OPTION="-delete -print"
+OPTION="-print -delete"
 
-# North LST arrays
-find "$1" -name "*NIM4LST4*3LSTs00MSTs-*" -print -delete
-find "$1" -name "*NIM4LST4*2LSTs00MSTs-*" -print -delete
-find "$1" -name "*NIM3LST3*2LSTs00MSTs-*" -print -delete
+##################
 # South LST arrays
-find "$1" -name "*NIM4LST4*3LSTs00MSTs00SSTs-*" -print -delete
-find "$1" -name "*NIM4LST4*2LSTs00MSTs00SSTs-*" -print -delete
-find "$1" -name "*NIM3LST3*2LSTs00MSTs00SSTs-*" -print -delete
-
-find "$1" -name "*NIM3LST3MST3*2LSTs03MSTs*" -print -delete
-find "$1" -name "*NIM4LST4MST4*2LSTs03MSTs*" -print -delete
+# 4 LSTs
+find "$1" -name "*NIM5LST5*4LSTs00MSTs00SSTs-*" ${OPTION}
+find "$1" -name "*NIM6LST6*4LSTs00MSTs00SSTs-*" ${OPTION}
+# 3 LSTs
+find "$1" -name "*NIM5LST5*3LSTs00MSTs00SSTs-*" ${OPTION}
+find "$1" -name "*NIM6LST6*3LSTs00MSTs00SSTs-*" ${OPTION}
+find "$1" -name "*NIM4LST4*3LSTs00MSTs00SSTs-*" ${OPTION}
+# 2 LSTs
+find "$1" -name "*NIM5LST5*2LSTs00MSTs00SSTs-*" ${OPTION}
+find "$1" -name "*NIM6LST6*2LSTs00MSTs00SSTs-*" ${OPTION}
+find "$1" -name "*NIM4LST4*2LSTs00MSTs00SSTs-*" ${OPTION}
+find "$1" -name "*NIM3LST3*2LSTs00MSTs00SSTs-*" ${OPTION}
+# 2LST 3 MSTs
+find "$1" -name "*NIM3LST3MST3*2LSTs03MSTs*" ${OPTION}
+find "$1" -name "*NIM4LST4MST4*2LSTs03MSTs*" ${OPTION}
 
 # South SST arrays only
-find "$1" -name "*ID2*-[0-9][0-9]SSTs.*" -print -delete
+find "$1" -name "*ID2*-[0-9][0-9]SSTs.*" ${OPTION}
 
 # South MST arrays only
-find "$1" -name "*ID3*-[0-9][0-9]MSTs-MSTF*" -print -delete
+find "$1" -name "*ID3*-[0-9][0-9]MSTs-MSTF*" ${OPTION}
+
+# North LST arrays
+find "$1" -name "*NIM4LST4*3LSTs00MSTs-*" ${OPTION}
+find "$1" -name "*NIM4LST4*2LSTs00MSTs-*" ${OPTION}
+find "$1" -name "*NIM3LST3*2LSTs00MSTs-*" ${OPTION}
