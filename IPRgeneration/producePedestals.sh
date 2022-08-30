93c104907b630f99e9d1c99f95669872abc1426e
Orel Gueta <22728856+orelgueta@users.noreply.github.com>
2022-08-30 12:39:07 +0200

    Modify for Prod6.

--- IPRgeneration/producePedestals.sh
+++ IPRgeneration/producePedestals.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Produce pedestal files from NSB simulations using sim_telarray
 #
-# script prepared for prod5 simulations
+# script prepared for prod6 simulations
 #
 # Change the two variables at the top:
 #  SIM_TELARRAY_PATH: pointing towards your sim_telarray installation
@@ -12,7 +12,7 @@
 
 CDIR=$(pwd)
 SIM_TELARRAY_PATH=$SIM_TELARRAY_PATH # Change this if you have your own sim_telarray and do not use the setupPackage.sh script
-SCRATCH="$CTA_USER_DATA_DIR/tmp/"
+SCRATCH="."
 
 ZENITH="20.0"
 # ZENITH="60.0"
@@ -20,15 +20,18 @@ ZE=${ZENITH%.*} # For the file names
 
 echo "Zenith angle set to ${ZENITH}"
 
-MOONOPT="-DFULLMOON" # Set to -DHALFMOON for half moon, -DFULLMOON for full moon or leave empty or dark conditions (i.e., MOONOPT="")
+MOONOPT="" # Set to -DHALFMOON for half moon, -DFULLMOON for full moon or leave empty or dark conditions (i.e., MOONOPT="")
 MOON=`echo "${MOONOPT#*-D}" | tr "[:upper:]" "[:lower:]"`
 
 [ -z "$MOON" ] && echo "Running dark conditions" || MOON="-${MOON}"
 
-TELTYPES=( LST MST-FlashCam MST-NectarCam SST )
-TELTYPES=( SST )
+TELTYPES=( LST MST-FlashCam MST-NectarCam SST SCT MAGIC)
+SITE=( CTA_NORTH CTA_SOUTH CTA_NORTH CTA_SOUTH CTA_SOUTH CTA_NORTH )
 PROD="PROD4"
 PROD="PROD5"
+PROD="PROD6"
+
+
 
 # dedicated scratch directory
 SCRATCH=${SCRATCH}/${PROD}/ze-${ZE}${MOON}
@@ -36,8 +39,9 @@ mkdir -p ${SCRATCH}
 echo "Writing all data products to ${SCRATCH}"
 echo "(use this directory as input for all following analysis steps)"
 
-for T in "${TELTYPES[@]}"
+for i in "${!TELTYPES[@]}"
 do
+    T="${TELTYPES[$i]}"
     echo "Simulating $T"
 
     outputFile="${SCRATCH}/pedestals-${T}${MOON}-ze-${ZE}-1k.simtel.gz"
@@ -46,7 +50,7 @@ do
     CFG="${SIM_TELARRAY_PATH}/cfg/CTA/CTA-${PROD}-${T}.cfg"
 
     ${SIM_TELARRAY_PATH}/bin/sim_telarray -c ${CFG} \
-       -I${SIM_TELARRAY_PATH}/cfg/CTA -I${SIM_TELARRAY_PATH}/cfg/common -I${SIM_TELARRAY_PATH}/cfg/hess -C Altitude=2150 -C iobuf_maximum=1000000000 \
+       -I${SIM_TELARRAY_PATH}/cfg/CTA -I${SIM_TELARRAY_PATH}/cfg/common -I${SIM_TELARRAY_PATH}/cfg/hess -D"${SITE[$i]}" -C Altitude=2150 -C iobuf_maximum=1000000000 \
        ${MOONOPT} -DNUM_TELESCOPES=1 -C maximum_telescopes=1 \
        -C atmospheric_transmission=atm_trans_2150_1_10_0_0_2150.dat \
        -DNSB_AUTOSCALE -C telescope_theta=${ZENITH} -C telescope_phi=180 \
