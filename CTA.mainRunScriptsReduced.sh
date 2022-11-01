01730321d6fe0df29c502bc424165dd98a77d9af
GernotMaier <gernot.maier@desy.de>
2022-11-01 08:26:32 +0100

    HTCondor submission for PHYS; improved log file handling

--- CTA.mainRunScriptsReduced.sh
+++ CTA.mainRunScriptsReduced.sh
@@ -7,7 +7,7 @@
 
 if [ $# -lt 2 ]; then
     echo "
-./CTA.mainRunScripts.hs <data set> <run mode>
+./CTA.mainRunScripts.sh <data set> <run mode>
     
     data sets:
         prod5-South-20deg prod5-South-40deg prod5-South-60deg
@@ -34,6 +34,10 @@ if [[ ${P2} == *"North"* ]]; then
    NMULT=( 2 3 4 )
 fi
 LSTMULT=( 2 3 4 )
+
+# run scripts are collected here
+RUNSCRIPTDIR="${CTA_USER_LOG_DIR}/jobs/$(uuidgen)"
+mkdir -p ${RUNSCRIPTDIR}
     
 if [[ ${RUN} == "MAKETABLES" ]] || [[ ${RUN} == "DISPBDT" ]] || [[ ${RUN} == "ANATABLES" ]] || [[ ${RUN} == "PREPARETMVA" ]]; then
    ./CTA.runAnalysis.sh ${P2} ${RUN}
@@ -41,8 +45,11 @@ if [[ ${RUN} == "MAKETABLES" ]] || [[ ${RUN} == "DISPBDT" ]] || [[ ${RUN} == "AN
 else
     while IFS= read -r mult
     do
-        ./CTA.runAnalysis.sh ${P2} ${RUN} 0 $mult
-    done < NIM-South.txt
+        ./CTA.runAnalysis.sh ${P2} ${RUN} 0 $mult ${RUNSCRIPTDIR}
+    done < NIM-South-test.txt
+    # tmp
+    exit
+    # tmp(end)
     while IFS= read -r mult
     do
         ./CTA.runAnalysis.sh ${P2}-sub ${RUN} 0 $mult
--- CTA.runAnalysis.sh
+++ CTA.runAnalysis.sh
@@ -11,7 +11,7 @@ if [ $# -lt 2 ]
 then
    echo "
    ./CTA.runbAnalysis.sh <S/S40deg> <run mode> [recid] \\\\
-                [min number of LSTs] [min number of MSTs] [min number of SSTs] [min number of SCMSTs]
+                [min number of LSTs] [min number of MSTs] [min number of SSTs] [min number of SCMSTs] [job_dir]
    
     Prod3b analysis:
          prod3b-S20deg / prod3b-S40deg / prod3b-S60deg
@@ -31,7 +31,8 @@ then
     possible run modes are EVNDISP MAKETABLES DISPBDT/DISPMLP ANATABLES PREPARETMVA TRAIN ANGRES QC CUTS PHYS 
    
     [recids]: 0 = all telescopes (default), 1 = LSTs, 2 = MSTs, 3 = SSTs, 4 = MSTs+SSTs, 5 = LSTs+MSTs
-    [XST multiplicities]: default = 2
+
+    [job_dir]: run scripts and job files are written to this directory
    "
    
    exit
@@ -48,6 +49,7 @@ RUN="$2"
 [[ "$5" ]] && MST=$5 || MST="2"
 [[ "$6" ]] && SST=$6 || SST="2"
 [[ "$7" ]] && SCMST=$7 || SCMST="2"
+[[ "$8" ]] && PDIR=${8} || PDIR="$CTA_USER_LOG_DIR/"
 echo "Telescope multiplicities: LST ${LST} MST ${MST} SST ${SST} SCMST ${SCMST}"
 
 #####################################
@@ -57,8 +59,7 @@ QSUBOPT="_M_P_X_cta_high_X__M_js_X_9"
 
 #####################################
 # output directory for script parameter files
-PDIR="$CTA_USER_LOG_DIR/tempRunParameterDir/"
-mkdir -p "$PDIR"
+mkdir -p "$PDIR/tempRunParameterDir/"
 
 #####################################
 # analysis dates and table dates
@@ -271,6 +272,7 @@ then
    fi
    ARRAY=( "subArray.prod5.South-BL.list" )
    ARRAY=( "subArray.prod5.South-Alpha.list" )
+   ARRAY=( "subArray.prod5.South-Alpha-Test.list" )
    if [[ $P2 == *"sub"* ]]; then
        ARRAY=( "subArray.prod5.South-Alpha-sub.list" )
    fi
@@ -290,12 +292,13 @@ then
       ARRAY=( "subArray.prod5.South-SV0.list" )
    fi
    ARRAYDIR="prod5"
-   TDATE="g20220408"
+   TDATE="g20220902"
    ANADATE="${TDATE}"
    ANADATE="g20220609"
    TMVADATE="${ANADATE}"
    EFFDATE="${ANADATE}"
    PHYSDATE="${EFFDATE}"
+   PHYSDATE="g20221031"
 ####################################
 # prod6 - Paranal and LaPalma
 elif [[ $P2 == "prod6"* ]]
@@ -417,7 +420,7 @@ if [[ ! -e ${ARRAYDIR}/$ARRAY ]]; then
    exit
 fi
 NXARRAY=$(cat ${ARRAYDIR}/$ARRAY)
-NFILARRAY=$PDIR/temp.$ARRAY.list
+NFILARRAY=$PDIR/tempRunParameterDir/temp.$ARRAY.list
 rm -f "$NFILARRAY"
 touch "$NFILARRAY"
 for A in $NXARRAY
@@ -538,7 +541,7 @@ do
                   then
                       TMVATYPF=NIM${NIMAGESMIN}LST${LST}MST${MST}SST${SST}
                   fi
-                  PARA="$PDIR/scriptsInput.${ID}${ETYPF}${AZ}.${SITE}${AZ}${OOTIME}.runparameter"
+                  PARA="$PDIR/tempRunParameterDir/scriptsInput.${ID}${ETYPF}${AZ}.${SITE}${AZ}${OOTIME}.runparameter"
                   rm -f "$PARA"
                   touch "$PARA"
                   echo "WRITING PARAMETERFILE $PARA"
@@ -680,6 +683,7 @@ do
                                 ${SITE}${EDM} \
                                 $BFINEBINNING \
                                 $PHYSDATE \
+                                ${PDIR} \
                                 $QSUBOPT
                      else
                         ./CTA.WPPhysWriter.sub.sh \
@@ -692,6 +696,7 @@ do
                                 ${SITE}${EDM} \
                                 $BFINEBINNING \
                                 $PHYSDATE \
+                                ${PDIR} \
                                 $QSUBOPT
                  fi
 # unknown run set
