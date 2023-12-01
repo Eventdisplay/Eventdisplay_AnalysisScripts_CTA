e847de4538a28607d0a7b8b064bd33c7e47feb75
GernotMaier <gernot.maier@desy.de>
2023-12-01 07:59:01 +0100

    improved dl2 converter script

--- convertDL2/convert_to_DL2.sh
+++ convertDL2/convert_to_DL2.sh
@@ -5,8 +5,9 @@
 
 if [ $# -lt 2 ]; then
    echo "
-./convert_to_DL2.sh <directory with effective area files> <paranal/lapalma>
+./convert_to_DL2.sh <directory with effective area files> <list of arrays>
    "
+   exit
 fi
 
 # check if converter script exists
@@ -16,26 +17,31 @@ if [[ ! -e ${EVNDISPSYS}/../Converters/DL2/generate_DL2_file.py ]]; then
    exit
 fi
 
-# list of effective area files
-FLIST=$(find ${1} -name "*.root")
+# list of arrays
+ARRAYS=$(cat $3)
 
-# Loop over all cut levels and convert the files
-for C in 0 1 2 
+for A in $ARRAYS
 do
-   for F in $FLIST
-   do
-      if [[ ! -e ${F} ]]; then
-         echo "Error: file not found: $F"
-         continue;
-      fi
+    # list of effective area files
+    FLIST=$(find ${1} -path "*BDT*" -name "*${A}*.root")
 
-      OFIL=$(basename $F .root)
-      OFIL=${OFIL}-CUT${C}.fits.gz
+    # Loop over all cut levels and convert the files
+    for C in 0 1 2 
+    do
+       for F in $FLIST
+       do
+          if [[ ! -e ${F} ]]; then
+             echo "Error: file not found: $F"
+             continue;
+          fi
 
-      OPTIONS="${2} -c ${C} -o ${1}/${OFIL}"
-      echo "CONVERTING ${F}"
-      python ${EVNDISPSYS}/../Converters/DL2/generate_DL2_file.py ${F} ${OPTIONS}
-      echo "---------------"
-   done
+          OFIL=$(basename $F .root)
+          OFIL=${OFIL}-CUT${C}.fits.gz
+
+          OPTIONS="-c ${C} -o ${1}/${OFIL} -l ${A}"
+          echo "CONVERTING ${F}"
+          python ${EVNDISPSYS}/../Converters/DL2/generate_DL2_file.py ${OPTIONS} ${F}
+          echo "---------------"
+       done
+    done
 done
-
