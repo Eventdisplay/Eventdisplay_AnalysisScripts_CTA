c7596af21692ffd200a11922fcb2fc8654d5b93b
GernotMaier <gernot.maier@desy.de>
2025-05-12 14:22:16 +0200

    correct docstring / help

--- IPRgeneration/convertToDST.sh
+++ IPRgeneration/convertToDST.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# Produce IPR graphs from NSB simulations
-# Converts first simtel_array ouput file to DST
+# Converts first simtel_array ouput file to DST for IPR generation
 #
 # Requires an Eventdisplay installation
 # including all environmental variables
@@ -8,20 +7,13 @@
 
 if [ $# -lt 1 ]; then
     echo "
-./produceIPRGraphs.sh <directory with simtel files> [production (default=PROD6; optional PROD5)
+./convertToDST.sh <directory with simtel files> [production (default=PROD6; optional PROD5)
     "
     exit
 fi
 SCRATCH=${1}
 [[ "$2" ]] && PROD=$2 || PROD="PROD6"
 CDIR=$(pwd)
-
-if [[ $PROD == "PROD5" ]]; then
-    RUNPARA="EVNDISP.prod5.reconstruction.runparameter"
-else
-    RUNPARA="EVNDISP.prod6.reconstruction.runparameter"
-fi
-echo "Using ${RUNPARA} for Production ${PROD}"
 
 FLIST=$(find $SCRATCH -name "*.simtel.gz")
 
